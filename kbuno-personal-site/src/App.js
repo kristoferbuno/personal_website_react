bcbbd692f516eaf97c2c727001e9a277ee885fae
kristoferbuno <krisbuno@gmail.com>
2021-06-16 19:40:05 -0400

    Change styling

--- kbuno-personal-site/src/App.js
+++ kbuno-personal-site/src/App.js
@@ -1,59 +1,65 @@
 import React, { Component } from 'react';
-import { Card, Typography, CardContent, Grid } from '@material-ui/core';
+import { Card, Typography, CardContent, Grid, Container } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  renderMainCards() {
+    return <Grid container item md={6} spacing={2}>
+    <Grid item md={6}>
+      <Card>
+        <CardContent>
+          <Typography>
+            about me
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+    <Grid item md={6}>
+      <Card>
+      <CardContent>
+          <Typography>
+            projects
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+    <Grid item md={6}>
+      <Card>
+      <CardContent>
+          <Typography>
+            resume download
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+    <Grid item md={6}>
+      <Card>
+      <CardContent>
+          <Typography>
+            hi
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+  }
+
   render() {
     return (
       <Grid container xs={12}>
-        <Grid item lg={6}>
+        <Grid item sm={6} xs={12}>
           <Card>
             <CardContent>
-              <Typography>
-                kristofer buno
-              </Typography>
+              <Container align="center">
+                <Typography align="center">
+                  kristofer buno
+                </Typography>
+              </Container>
             </CardContent>
           </Card>
         </Grid>
-        <Grid container item lg={6} spacing={2}>
-          <Grid item lg={6}>
-            <Card>
-              <CardContent>
-                <Typography>
-                  kristofer buno
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item lg={6}>
-            <Card>
-            <CardContent>
-                <Typography>
-                  kristofer buno
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item lg={6}>
-            <Card>
-            <CardContent>
-                <Typography>
-                  kristofer buno
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item lg={6}>
-            <Card>
-            <CardContent>
-                <Typography>
-                  kristofer buno
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
+        {this.renderMainCards()}
       </Grid>
     );
   }
