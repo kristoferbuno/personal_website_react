cee4f369a266c1c44deec66abc740932b9c670d3
kristoferbuno <krisbuno@gmail.com>
2021-06-16 21:44:44 -0400

    Add buttons for about me, projects

--- kbuno-personal-site/src/App.js
+++ kbuno-personal-site/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Card, Typography, CardContent, Grid, Container } from '@material-ui/core';
+import { Card, Typography, CardContent, Grid, Container, CardActionArea } from '@material-ui/core';
 import "@fontsource/source-code-pro"
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import FaceIcon from '@material-ui/icons/Face';
 import BuildIcon from '@material-ui/icons/Build';
 import ReceiptIcon from '@material-ui/icons/Receipt';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import './App.css';
 
 const theme = createMuiTheme({
@@ -17,30 +18,54 @@ const theme = createMuiTheme({
 });
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      active_panel: null
+    }
+  }
+
+  handlePanelChange(panel) {
+    console.log(panel)
+    this.setState({active_panel: panel});
+  }
+
+  fakeFunction() {
+    console.log("I am here for debugging purposes.")
+  }
+
   renderMainCards() {
     return <Grid container item md={6} spacing={2}>
     <Grid item xs={6}>
       <Card>
-        <CardContent>
-        <Grid container direction="row" alignItems="center">
-            <FaceIcon />
-            <Typography style={{marginLeft: '0.5em'}}>
-              about me
-            </Typography>
-        </Grid>
-        </CardContent>
+        <CardActionArea onClick={() => {
+          this.handlePanelChange('about_me')
+        }}>
+          <CardContent>
+            <Grid container direction="row" alignItems="center">
+                <FaceIcon />
+                <Typography style={{marginLeft: '0.5em'}}>
+                  about me
+                </Typography>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Grid>
     <Grid item xs={6}>
       <Card>
-        <CardContent>
-          <Grid container direction="row" alignItems="center">
-            <BuildIcon />
-            <Typography style={{marginLeft: '0.5em'}}>
-              projects
-            </Typography>
-          </Grid>
-        </CardContent>
+      <CardActionArea onClick={() => {
+          this.handlePanelChange('projects')
+        }}>
+          <CardContent>
+            <Grid container direction="row" alignItems="center">
+              <BuildIcon />
+              <Typography style={{marginLeft: '0.5em'}}>
+                projects
+              </Typography>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Grid>
     <Grid item xs={6}>
@@ -67,6 +92,116 @@ class App extends Component {
   </Grid>
   }
 
+  renderAboutMe() {
+    return <Grid item md={6}>
+    <Grid item xs={12}>
+      <Card>
+        <CardActionArea onClick={() => {this.handlePanelChange(null)}}>
+          <CardContent>
+            <Grid container direction="row" alignItems="center">
+                <ArrowBackIcon />
+                <Typography style={{marginLeft: '0.5em'}}>
+                  back
+                </Typography>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
+        <CardContent id="about_me_content">
+          <Typography>
+              hi, i'm kristofer buno
+          </Typography>
+          <Typography>
+              i'm a cs student at the university of florida, planning to graduate in either dec 2022 or may 2023
+          </Typography>
+          <Typography>
+              i like to wear many hats - i learn technologies quick to focus on the big picture of making solutions
+          </Typography>
+          <Typography>
+              i love lifting weights, cooking, snowboarding, longboarding, and getting into new sports
+          </Typography>
+          <Typography>
+              if you want to get to know me better, talk to me!
+          </Typography>
+          </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+  }
+
+  renderProjects() {
+    return <Grid item md={6}>
+    <Grid item xs={12}>
+      <Card>
+        <CardActionArea onClick={() => {this.handlePanelChange(null)}}>
+          <CardContent>
+            <Grid container direction="row" alignItems="center">
+                <ArrowBackIcon />
+                <Typography style={{marginLeft: '0.5em'}}>
+                  back
+                </Typography>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
+        <CardContent id="about_me_content">
+          <Typography>
+              [0] song analyzer
+              <Typography variant='body2' className="indented">
+              $ analyzes appropriateness of a song in 4 categories
+              </Typography>
+              <Typography variant='body2' className="indented">
+              $ uses machine learning techniques to classify songs
+              </Typography>
+              <Typography variant='body2' className="indented">
+              $ built with react, js, python, docker, aws
+              </Typography>
+          </Typography>
+          <Typography>
+              [1] uf ap credit calculator
+              <Typography variant='body2' className="indented">
+              $ calculates earned uf credit from ap exams
+              </Typography>
+              <Typography variant='body2' className="indented">
+              $ built with js, html5 and dom manipulation
+              </Typography>
+          </Typography>
+          <Typography>
+              [2] stayin' alive
+              <Typography variant='body2' className="indented">
+              $ educates users on cpr with a Unity3D VR simulation
+              </Typography>
+              <Typography variant='body2' className="indented">
+              $ built with Unity3D and C#
+              </Typography>
+          </Typography>
+          <Typography>
+              [3] personal website
+              <Typography variant='body2' className="indented">
+              $ simple website to organize my info
+              </Typography>
+              <Typography variant='body2' className="indented">
+              $ built with react
+              </Typography>
+              <Typography variant='body2' className="indented">
+              $ you're looking at it right now
+              </Typography>
+          </Typography>
+          </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+  }
+
+  renderActivePanel() {
+    switch (this.state.active_panel) {
+      case null:
+        return;
+      case 'about_me':
+        return this.renderAboutMe();
+      case 'projects':
+        return this.renderProjects();
+    }
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={theme}>
@@ -85,7 +220,7 @@ class App extends Component {
               </CardContent>
             </Card>
           </Grid>
-          {this.renderMainCards()}
+          {this.state.active_panel ? this.renderActivePanel() : this.renderMainCards()}
         </Grid>
       </MuiThemeProvider>
     );
